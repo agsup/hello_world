5f51d7bc744704d53d573374a9ce888dbbc97b63
agsup <75041507+agsup@users.noreply.github.com>
2021-11-09 15:42:39 -0600

    Update doo.sh

--- doo.sh
+++ doo.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sudo pacman -S nemo xfce4-terminal steam etcher libreoffice
+sudo pacman -S nemo xfce4-terminal steam etcher libreoffice cinnamon npm gdb
 sudo ln /snap /var/lib/snapd/snap --symbolic
