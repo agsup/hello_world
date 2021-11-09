1a1626f6d65bde30c18eda359272f805c4e29964
agsup <75041507+agsup@users.noreply.github.com>
2021-11-09 16:39:47 -0600

    Add files via upload

--- doo.sh
+++ doo.sh
@@ -1,3 +1,20 @@
-#!/bin/bash
-sudo pacman -S nemo xfce4-terminal steam etcher libreoffice cinnamon npm gdb lolcat oh-my-zsh
+sudo pacman -S nemo xfce4-terminal steam etcher libreoffice cinnamon npm gdb lolcat solaar oh-my-zsh
 sudo ln /snap /var/lib/snapd/snap --symbolic
+snap install code --classic
+mkdir font_gits
+mkdir imported_fonts
+cd font_gits
+git clone https://github.com/njugunagathere/Metropolis.git
+git clone https://github.com/googlefonts/atkinson-hyperlegible.git
+git clone https://github.com/googlefonts/spacemono.git
+cd atkinson-hyperlegible
+cp -r fonts ../../imported_fonts
+cd ..
+cp -r Metropolis ../imported_fonts
+cd spacemono
+cp -r fonts ../../imported_fonts
+cd ..
+
+cd ..
+
+sudo cp -r imported_fonts /usr/share/fonts
